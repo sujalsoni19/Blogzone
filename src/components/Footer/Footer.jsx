4e0359c793ce62ec34c933f2e9e7c87b9f7d9d8e
sujalsoni19 <sujal19soni2004@gmail.com>
2026-01-04 21:49:51 +0530

    fixed year logic in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ function Footer() {
                             </div>
                             <div>
                                 <p className="text-sm ">
-                                    &copy; Copyright 2025. All Rights Reserved by Blogzone.
+                                    &copy; Copyright {new Date().getFullYear()}. All Rights Reserved by Blogzone.
                                 </p>
                             </div>
                         </div>
